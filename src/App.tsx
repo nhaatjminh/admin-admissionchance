eaf4c9a7450dd4e6821f564db9d5869c1a8ea2b2
nhaatjminh <minhphan.40199@gmail.com>
2024-01-27 17:35:22 +0700

    hide appsmith logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useLayoutEffect, useRef } from 'react'
 import './App.css'
+import yootLogo from './assets/YootLogo.png'
 
 function App() {
+  const ref = useRef<HTMLIFrameElement>(null)
+
+  useLayoutEffect(() => {
+    if (!ref.current) return
+    ref.current.onload = () => {
+      console.log('loaded')
+      const contentWindow = ref.current?.contentWindow
+      const item = contentWindow?.document.querySelector('hidden')
+      console.log('🚀🚀', item)
+      // item?.remove()
+    }
+  }, [ref])
   return (
     <>
-      <iframe className='iframe-admin_admissionchance' src={import.meta.env.VITE_ADMIN_ADMISSIONCHANCE_UL || ''} />
+      <iframe
+        ref={ref}
+        className='iframe-admin_admissionchance'
+        src={import.meta.env.VITE_ADMIN_ADMISSIONCHANCE_UL || ''}
+      />
+      <div className='yoot-logo'>
+        <img src={yootLogo} />
+      </div>
     </>
   )
 }
